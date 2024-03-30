8d02fd0bb7d774abf8eb394d2a21c4a7b8934313
Pratyush Harsh <pratyush9000@gmail.com>
2024-03-30 21:49:44 +0530

    forward and backward updated 2

--- src/EANN.py
+++ src/EANN.py
@@ -46,15 +46,14 @@ class Rumor_Data(Dataset):
 
 
 class ReverseLayerF(Function):
-
-    #@staticmethod
-    def forward(self, x):
-        self.lambd = args.lambd
+    @staticmethod
+    def forward(ctx, x, lambd):
+        ctx.lambd = lambd
         return x.view_as(x)
 
-    #@staticmethod
-    def backward(self, grad_output):
-        return (grad_output * -self.lambd)
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output * -ctx.lambd, None
 
 def grad_reverse(x):
     return ReverseLayerF()(x)
